a22c14b65f7acb24c3f961f96100b9a8ca7e9111
kepo213 <101172343+kepo213@users.noreply.github.com>
2022-05-06 02:27:05 +0700

    Update misc.py

--- misc.py
+++ misc.py
@@ -6,9 +6,7 @@ from aiogram.dispatcher import FSMContext
 import json
 
 
-TOKEN = ('5126196895:AAFaKCWX9lI5GPfkg2vW55fJa_U9y8-Lrfo')
-
 memory_storage = MemoryStorage()
-bot = Bot(token=TOKEN, parse_mode='html', disable_web_page_preview=True)
+bot = Bot(token="5317907528:AAF-S4YcbUNo_XcX6caSDI6GiXnsQcKCY3c", parse_mode='html', disable_web_page_preview=True)
 dp = Dispatcher(bot,storage=memory_storage)
 logging.basicConfig(level=logging.INFO)
