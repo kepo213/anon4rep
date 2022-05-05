51a06f8cc1fe3b6923bedfe7573dcc7017e2c8e9
kepo213 <101172343+kepo213@users.noreply.github.com>
2022-05-06 03:34:19 +0700

    Update anon4bot.py

--- anon4bot.py
+++ anon4bot.py
@@ -1,6 +1,8 @@
 from aiogram import executor
 from misc import dp
 import handlers
+from aiogram.types import InlineQuery, \
+    InputTextMessageContent, InlineQueryResultArticle
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
