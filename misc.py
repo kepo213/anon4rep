42f80824d7a3deaaa0729ec5356879a1d6e29b02
kepo213 <101172343+kepo213@users.noreply.github.com>
2022-05-06 03:21:28 +0700

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,7 +1,6 @@
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 import json
@@ -12,6 +11,4 @@ API_TOKEN = "5317907528:AAF-S4YcbUNo_XcX6caSDI6GiXnsQcKCY3c"
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
-
 logging.basicConfig(level=logging.INFO)
