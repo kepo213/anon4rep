60087d6cbf77e90c73f8ed95223d83cfb54f1ac2
kepo213 <101172343+kepo213@users.noreply.github.com>
2022-05-06 02:57:28 +0700

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,4 +1,5 @@
 import logging
+import dp
 from aiogram import Bot,Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
