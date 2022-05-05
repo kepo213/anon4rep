d9013ab88e00b8b72b462162b1954a2c5fc4cb76
kepo213 <101172343+kepo213@users.noreply.github.com>
2022-05-06 03:07:58 +0700

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,14 +1,13 @@
 import logging
-from aiogram import types
-from aiogram import Bot,Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 import json
 
-TOKEN = 'BOT TOKEN HERE'
 
-# Initialize bot and dispatcher
-bot = Bot(TOKEN, parse_mode=types.ParseMode.HTML)
+API_TOKEN = "BOT_TOKEN_HERE"
+
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot,storage=memory_storage)
 logging.basicConfig(level=logging.INFO)
