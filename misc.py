53b0be98f5847664bd2a832abf35bb5c02e6304f
kepo213 <101172343+kepo213@users.noreply.github.com>
2022-05-06 02:29:26 +0700

    Update misc.py

--- misc.py
+++ misc.py
@@ -7,6 +7,6 @@ import json
 
 
 memory_storage = MemoryStorage()
-bot = Bot(token="5317907528:AAF-S4YcbUNo_XcX6caSDI6GiXnsQcKCY3c", parse_mode='html', disable_web_page_preview=True)
+bot = Bot("token="5317907528:AAF-S4YcbUNo_XcX6caSDI6GiXnsQcKCY3c", parse_mode='html', disable_web_page_preview=True")
 dp = Dispatcher(bot,storage=memory_storage)
 logging.basicConfig(level=logging.INFO)
